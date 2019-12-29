0953135d87ccd90c3673bc4ae51fa4d3671903f2
phelipe <phelipe_teles@hotmail.com>
2019-12-29 14:46:59 -0300

    tests: renamed arcgis url builder test

--- tests/test_arcgis_url.py
+++ tests/test_arcgis_url.py
@@ -10,13 +10,13 @@ from mapsbr import arcgis
 class TestArcGIS(unittest.TestCase):
 
     def test_build_url(self):
-        correct = "https://mapasinterativos.ibge.gov.br/arcgis/rest/services/BIOMA/MapServer/0/query?where=objectId>=0+and+objectId<1000&f=geojson"
         test = arcgis.build_url("BIOMA")
+        correct = "https://mapasinterativos.ibge.gov.br/arcgis/rest/services/BIOMA/MapServer/0/query?where=objectId>=0+and+objectId<1000&f=geojson"
         self.assertEqual(test, correct)
 
     def test_build_url_different_baseurl(self):
-        correct = "https://pgeo3.rio.rj.gov.br/arcgis/rest/services/Basicos/mapa_basico_UTM/MapServer/0/query?where=objectId>=0+and+objectId<1000&f=geojson"
         test = arcgis.build_url("Basicos/mapa_basico_UTM", baseurl="https://pgeo3.rio.rj.gov.br/arcgis/rest/services")
+        correct = "https://pgeo3.rio.rj.gov.br/arcgis/rest/services/Basicos/mapa_basico_UTM/MapServer/0/query?where=objectId>=0+and+objectId<1000&f=geojson"
         self.assertEqual(test, correct)
 
 
