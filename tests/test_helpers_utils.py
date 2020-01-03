e547e879f592a8ac53d57c041fd93b5661ba0624
phelipe <phelipe_teles@hotmail.com>
2020-01-02 23:08:49 -0300

    added test for charset converter

--- tests/test_helpers_utils.py
+++ tests/test_helpers_utils.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import unittest
+from unittest.mock import patch, Mock
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
@@ -27,6 +28,11 @@ class TestUtils(unittest.TestCase):
         D = {"invalid": "geojson"}
         with self.assertRaises(KeyError):
             utils.get_features(D)
+
+    def test_from_iso_to_utf8(self):
+        test = utils.from_iso88591_to_utf8("mÃ©dio")
+        correct = "médio"
+        self.assertEqual(test, correct)
 
 
 if __name__ == "__main__":
